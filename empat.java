0be987daf94d6d450bd0e4ee064ae3b3ddf577f8
Achmad Fauzi Makharim <alamnoh10@yahoo.com>
2010-10-24 17:20:10 +0700

    Keempat
    Switch Case

---
+++ empat.java
@@ -0,0 +1,30 @@
+public class tigasc {
+    public static void main(String args[]) {
+
+    	int pilih=10;
+       switch(pilih)
+    {
+        case 1: System.out.println("SATU");
+        break;
+        case 2: System.out.println("DUA");
+        break;
+        case 3: System.out.println("TIGA");
+        break;
+		case 4: System.out.println("EMPAT");
+		break;
+		case 5: System.out.println("LIMA");
+		break;
+		case 6: System.out.println("ENAM");
+		break;
+		case 7: System.out.println("TUJUH");
+		break;
+		case 8: System.out.println("DELAPAN");
+		break;
+		case 9: System.out.println("SEMBILAN");
+		break;
+        default: System.out.println("Nomer Yang anda masukkan Salah");
+        break;
+    }
+    System.exit(0);
+    }
+}
