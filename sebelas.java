c972024c6f2559db2265a46cc668818593b86f80
Achmad Fauzi Makharim <alamnoh10@yahoo.com>
2010-10-25 00:28:59 +0700

    Kesebelas
    kotak dgn karakter # yg genap di tab satu spasi
    panjang dan lebarnya sama

---
+++ sebelas.java
@@ -0,0 +1,19 @@
+public class sebelas {
+	public static void main(String args[]){
+
+		int a;
+		int b;
+		int c=10;
+
+		for(a=1;a<=c;a++){
+			if(a%2==0){
+				System.out.print(" ");
+			}
+			for(b=1;b<=c;b++){
+				System.out.print("#");
+			}
+			System.out.print("\n");
+		}
+	
+	}
+}
