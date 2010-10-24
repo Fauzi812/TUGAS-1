6e3e95f6850255589383702e5cca594aed8d0410
Achmad Fauzi Makharim <alamnoh10@yahoo.com>
2010-10-24 17:41:04 +0700

    Keenam
    Mencari jumlah dari deretan

--- enam.java
+++ enam.java
@@ -1,14 +1,16 @@
-public class lima {
+public class enam {
 	public static void main(String args[]){
 
 		int awal=1;
 		int akhir=10;
+		int jumlah=0;
 		System.out.println("Angka Yang Pertama = "+awal);
 		System.out.println("Angka Yang Terakhir = "+akhir);
 
 		while(awal<=akhir){
-			System.out.println(+awal);
+			jumlah=jumlah+awal;
 			awal++;
 		}
+		System.out.println(jumlah);
 	}
 }
