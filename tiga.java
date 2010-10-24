3b0860491235c728b00a59e18b63d05f29572515
Achmad Fauzi Makharim <alamnoh10@yahoo.com>
2010-10-24 17:19:48 +0700

    Ketiga
    IF ELSE bersarang

---
+++ tiga.java
@@ -0,0 +1,31 @@
+public class tiga {
+	public static void main(String args[]){
+
+		int angka =15;
+		System.out.println("Angka yang dimasukkan = "+angka);
+		System.out.println("Keterangan : ");
+
+		if(angka==1){
+		System.out.println("SATU");
+	} else if(angka==2) {
+		System.out.println("DUA");
+	} else if(angka==3) {
+		System.out.println("TIGA");
+	} else if(angka==4) {
+		System.out.println("EMPAT");
+	} else if(angka==5) {
+		System.out.println("LIMA");
+	} else if(angka==6) {
+		System.out.println("ENAM");
+	} else if(angka==7) {
+		System.out.println("TUJUH");
+	} else if(angka==8) {
+		System.out.println("DELAPAN");
+	} else if(angka==9) {
+		System.out.println("SEMBILAN");
+	} else {
+		System.out.println("NOMER YANG ANDA MASUKKAN SALAH");
+	}
+
+	}
+}
