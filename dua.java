addc84587a11a641b218be6faa14389da29d0fb8
Achmad Fauzi Makharim <alamnoh10@yahoo.com>
2010-10-24 17:18:48 +0700

    Kedua
    Mencari Ganjil Genap

---
+++ dua.java
@@ -0,0 +1,15 @@
+public class dua {
+	public static void main(String args[]){
+
+	int nilai=45789;
+	System.out.println("Nilai yang Anda masukkan = "+nilai);
+	System.out.println("Keterangan :");
+
+	if(nilai%2==0){
+		System.out.println("GENAP");
+	} else {
+		System.out.println("GANJIL");
+	}
+
+	}
+}
