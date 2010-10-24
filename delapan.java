a0c888d0b92d9596fa1b1c7c94f9fcecba1d4cae
Achmad Fauzi Makharim <alamnoh10@yahoo.com>
2010-10-25 00:26:20 +0700

    Kedelapan
    angka 1-110
    per baris 11

---
+++ delapan.java
@@ -0,0 +1,18 @@
+public class delapan {
+	public static void main(String args[]){
+
+		int awal=1;
+		int akhir=110;
+		int b;
+
+		for(awal=1;awal<=akhir;awal++){
+			System.out.print(awal);
+			if(awal<akhir){
+			System.out.print(" ");
+			if(awal%11==0){
+				System.out.print("\n");
+			}	
+			}
+		}
+	}
+}
