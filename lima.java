03af0ea6d23cdf1ffd52133e0428989f8009353c
Achmad Fauzi Makharim <alamnoh10@yahoo.com>
2010-10-24 17:20:41 +0700

    Kelima
    Deretan Angka 1 - 10

---
+++ lima.java
@@ -0,0 +1,14 @@
+public class lima {
+	public static void main(String args[]){
+
+		int awal=1;
+		int akhir=10;
+		System.out.println("Angka Yang Pertama = "+awal);
+		System.out.println("Angka Yang Terakhir = "+akhir);
+
+		while(awal<=akhir){
+			System.out.println(+awal);
+			awal++;
+		}
+	}
+}
