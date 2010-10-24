cef887ffd7bccaeb6a7080fe7a1e873cff57d739
Achmad Fauzi Makharim <alamnoh10@yahoo.com>
2010-10-24 17:18:24 +0700

    Pertama
    Mencari Tinggi badan

---
+++ satu.java
@@ -0,0 +1,16 @@
+public class satu {
+	public static void main(String args[]){
+
+	int Tinggi=150;
+	System.out.println("Tinggi Anda="+Tinggi);
+	System.out.println("Tinggi Yang Harus Dicapai = 100");
+	System.out.println("Keterangan :");
+
+	if(Tinggi<100){
+		System.out.println("Tidak Memenuhi Syarat");
+	} else {
+		System.out.println("Memenuhi Syarat");
+	}
+
+	}
+}
