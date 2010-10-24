569ff0755fca87b1bb837966e8bdbdd3b3e7cec8
Achmad Fauzi Makharim <alamnoh10@yahoo.com>
2010-10-24 17:42:54 +0700

    Keenam
    Jumlah Dari barisan deret

--- enam.java
+++ enam.java
@@ -2,10 +2,11 @@ public class enam {
 	public static void main(String args[]){
 
 		int awal=1;
-		int akhir=10;
+		int akhir=20;
 		int jumlah=0;
 		System.out.println("Angka Yang Pertama = "+awal);
 		System.out.println("Angka Yang Terakhir = "+akhir);
+		System.out.print("Jumlah Dari deretan angka diatas = ");
 
 		while(awal<=akhir){
 			jumlah=jumlah+awal;
