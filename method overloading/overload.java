8588b8d76623cc00e97ec226ccdd99834973461c
Achmad Fauzi Makharim <alamnoh10@yahoo.com>
2015-04-07 14:11:44 +0700

    method overloading
    nambah method overloading

--- method overloading/overload.java
+++ method overloading/overload.java
@@ -1,20 +1,25 @@
 public class overload {
-	public static int average(int n1, int n2){
+	public static int average(int n1, int n2){ //A
 		return (n1+n2)/2;
 	}
 
-	public static double average(double n1, double n2){
+	public static double average(double n1, double n2){ //B
 		return (n1+n2)/2;
 	}
 
-	public static int average(int n1, int n2, int n3){
+	public static int average(int n1, int n2, int n3){ //C
 		return (n1+n2+n3)/3;
 	}
 
+	public static int average(int n1, int n2, int n3, int n4){ //D
+		return (n1+n2+n3+n4)/4;
+	}
+
 	public static void main(String args[]){
-		System.out.println(average (1, 2));
-		System.out.println(average (1.0, 2.0));
-		System.out.println(average (1, 2, 3));
-		System.out.println(average (1.0, 2));
+		System.out.println(average (1, 2)); 			//A
+		System.out.println(average (1.0, 2.0));			//B
+		System.out.println(average (1, 2, 3));			//C
+		System.out.println(average (1.0, 2));			//B
+		System.out.println(average(1, 2, 3, 4));		//D
 	}
 }
